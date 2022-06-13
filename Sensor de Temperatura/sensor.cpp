a8b69d1b9666ac9d15f8248d2ab42724d212aea3
HenrSF <94074229+HenrSF@users.noreply.github.com>
2022-06-13 14:27:24 -0300

    Incompleto

---
+++ Sensor de Temperatura/sensor.cpp
@@ -0,0 +1,67 @@
+#include "sensor.hpp"
+
+SensorTemperatura::SensorTemperatura(){
+    //falta a unidade de medicao que o construtor recebera
+    ligado = false;
+    defeito = false;
+}
+
+void SensorTemperatura::ligar()
+
+{
+    ligado = true;
+}
+
+void SensorTemperatura::desligar()
+
+{
+    ligado = false;
+}
+
+void SensorTemperatura::setunidade(int u)
+
+{
+    
+}
+
+void SensorTemperatura::setdefeito(int d)
+
+{
+
+}
+
+/*static*/float SensorTemperatura::CelsiusToFahrenheit()
+
+{
+    F = (1.8*C) + 32;
+}
+
+/*static*/float SensorTemperatura::CelsiusToKelvin()
+
+{
+    K = C + 273;
+}
+
+/*static*/float SensorTemperatura::KelvinToCelsius()
+
+{
+    C = K - 273;
+}
+
+/*static*/float SensorTemperatura::KelvinToFahrenheit()
+
+{
+    F = ((K - 273)*1.8) + 32;
+}
+
+/*static*/float SensorTemperatura::FahrenheitToCelsius()
+
+{
+    F = (F - 32)/1.8;
+}
+
+/*static*/float SensorTemperatura::FahrenheitToKelvin()
+
+{
+    K = ((F-32)*(5/9)) + 273;
+}
---
+++ Sensor de Temperatura/sensor.hpp
@@ -0,0 +1,42 @@
+#ifndef CLASSE_SENSOR_
+#define CLASSE_SENSOR_
+using namespace std;
+#include <iostream>
+#include <string>
+
+class SensorTemperatura{
+    public:
+    SensorTemperatura (); //contrutor
+    
+    void setunidade(int);
+    void setdefeito(int);
+    
+    int getn_sensores();
+    int getcorrente_saida();
+    
+    void imprimir();    //imprime o estado do obj
+    void ligar();       //liga o sensor
+    void desligar();    //desliga o sensor
+    
+    static float CelsiusToKelvin();
+    static float CelsiusToFahrenheit();
+
+    static float KelvinToCelsius();
+    static float KelvinToFahrenheit();
+    
+    static float FahrenheitToCelsius();
+    static float FahrenheitToKelvin();
+    
+    int efetuaMedicao(); //medicao feita pelo sensor
+
+    private:
+    static int corrente_saida();
+    bool ligado = true;
+    static int n_sensores;
+    
+    int corrente_saida;
+    int defeito, unidade;
+
+};
+
+#endif
