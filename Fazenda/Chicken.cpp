1307bd5a8830b631a125e8cf0ef990ab873d302b
HenrSF <94074229+HenrSF@users.noreply.github.com>
2022-08-26 14:23:20 -0300

    Update Chicken.cpp

--- Fazenda/Chicken.cpp
+++ Fazenda/Chicken.cpp
@@ -7,16 +7,16 @@ namespace CHICKEN //Falta ajeitar;
 {
 float min_weight = 0.9;
 float max_weight = 1.8;
-int max_age = 30; //Days
-float food = 0.2;
-float walk = 0.001;
-string cry = "CÓ CÓ!";
+int max_age = 30;   //Days
+float food = 0.2;   //amount (kg) of weight this animal can gain
+float walk = 0.001; //distance travelled
+string cry = "CÓ CÓ!"; //onomatopoeia (portuguese - BR)
 }
 
 Chicken::Chicken(Gender gender, float weight, int age)
     : Animals (gender)
 {
-    //TODO: codigos adicionais do construtor
+    
     setweight(weight);
     setage(age);
     n_chickens++;
@@ -32,11 +32,11 @@ string Chicken::sound() const
     return CHICKEN::cry;
 }
 
-void Chicken::displacement()
+void Chicken::displacement() //everytime this function is called the animal will move a fixed distance.
 {
-    float w = getweight() - (getweight() * CHICKEN::walk);
+    float w = getweight() - (getweight() * CHICKEN::walk); //the animal loses weight at a fixed amount
 
-    if(w <= CHICKEN::min_weight)
+    if(w <= CHICKEN::min_weight) //the animal has a minimum weight value and it'll be set at 300 if any given value is below that
     setweight(w);
 }
 
@@ -44,7 +44,7 @@ void Chicken::setweight(float w)
 {
     if (w > CHICKEN::max_weight && w < CHICKEN::min_weight)
     {
-        Animals::setweight((CHICKEN::max_weight + CHICKEN::min_weight) / 2); //if the input value is invalid, it'll be setted has the mean between max and min values; 
+        Animals::setweight((CHICKEN::max_weight + CHICKEN::min_weight) / 2); //if the input value is invalid, it'll be set to have the mean between max and min values  
     }
 
     Animals::setweight(w);
@@ -59,30 +59,30 @@ void Chicken::setweight(float w)
 
 float Chicken::price()
 {
-    return getweight() * price_kg;
+    return getweight() * price_kg; //returns the revenue of an animal
 }
 
 void Chicken::eat()
 {
-    if(getweight() + CHICKEN::food >= CHICKEN::max_weight)
+    if(getweight() + CHICKEN::food >= CHICKEN::max_weight) //the animal's weight cannot be above the maximum
     {
         setoverweight();
     }
 
     else
     {
-        setweight(getweight() + CHICKEN::food);
+        setweight(getweight() + CHICKEN::food); //the animal gain a fixed amount of weight
     }
 }
 
 void Chicken::setage(int n)
 {
-    if(n > 0 && n <= CHICKEN::max_age)
+    if(n > 0 && n <= CHICKEN::max_age) //the animal has a maximum age which is going to be considered when or not it'll be killed
     {
         Animals::setage(n);
     }
 
-    else Animals::setage(1);
+    else Animals::setage(1); //if the input value is an invalid one. The age will be 1 (day)
 }
 void Chicken::print()
 {
