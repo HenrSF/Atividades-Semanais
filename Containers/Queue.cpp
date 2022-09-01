51e47525622ac5406491c5309d062697bc593338
Felliph-Silva <felliphnascimento17@outlook.com>
2022-08-31 23:41:26 -0300

    Containers

---
+++ Containers/Queue.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    queue <string> fila;
+
+    return 0;
+}
---
+++ Containers/Stack.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    stack <string> pilha;
+
+        pilha.push("Primeira posição");
+        pilha.push("Segunda posição");
+        pilha.push("Terceira posição");
+
+    return 0;
+}
